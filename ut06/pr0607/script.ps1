9bc6beda62d178a658d4bb594ec82e8afcb4e540
César Romera Benavente <cesar.romera.benavente@gmail.com>
2026-01-09 09:35:19 +0100

    yeah

--- ut06/pr0607/script.ps1
+++ ut06/pr0607/script.ps1
@@ -4,12 +4,27 @@ $nuevosEmpleadosProcesados = foreach ($linea in $nuevosempleadosraw){
 
 $partes = $linea -split '\|'
 
+# Nombre y apellidos
+
 $nombre = $partes[0].split(",")[1]
 $apellidos = $partes[0].split(",")[0]
 
 $nombreApellidos = (Get-Culture).TextInfo.ToTitleCase("$nombre $apellidos".ToLower())
 
-$nombreApellidos
+# Usuario
+
+$parteNombre = $nombre.Trim().Substring(0,1).ToLower()
+
+$apellidoLimpio = $apellidos.Split(" ")[0].Replace("'","").Trim()
+
+$largoCorte = [Math]::Min(6, $apellidoLimpio.Length)
+$parteApellido = $apellidoLimpio.Substring(0, $largoCorte).ToLower()
+
+$nombreUsuario = $parteNombre + $parteApellido
+
+$nombreUsuario
+
+
 
 }
 
