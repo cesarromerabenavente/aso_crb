aee5fe2c6847b90dd53abce4c800d7e00e12533c
César Romera Benavente <cesar.romera.benavente@gmail.com>
2026-01-09 09:16:30 +0100

    yeah

--- ut06/pr0607/script.ps1
+++ ut06/pr0607/script.ps1
@@ -1,4 +1,16 @@
 $nuevosempleadosraw = Get-Content -Path C:\ASO\ut06\pr0607\nuevos_empleados_raw.txt
 
-Write-Host "Este es el contenido del archivo"
-$nuevosempleadosraw
+$nuevosEmpleadosProcesados = foreach ($linea in $nuevosempleadosraw){
+
+$partes = $linea -split '\|'
+
+$nombre = $partes[0].split(",")[1]
+$apellidos = $partes[0].split(",")[0]
+
+$nombreApellidos = (Get-Culture).TextInfo.ToTitleCase("$nombre $apellidos".ToLower())
+
+$nombreApellidos
+
+}
+
+$nuevosEmpleadosProcesados
